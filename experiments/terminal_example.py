e7c519ee6a138ba3b04b3416cade89685ae9cd01
Aleksey Titov <staffofmousecorp@gmail.com>
2017-03-09 17:37:18 +0300

    Make speech-ai a class
    Refactor speech_ai.
    Change example terminal_example.py
    Delete unnecessary files.

--- experiments/terminal_example.py
+++ experiments/terminal_example.py
@@ -1,35 +1,21 @@
 # -*- coding: utf-8 -*-
 from chatterbot import ChatBot
-import logging
+from chatterbot.trainers import ChatterBotCorpusTrainer
+bot = ChatBot("Al",
+              logic_adapters=[{
+                  'import_path': 'chatterbot.logic.LowConfidenceAdapter',
+                  'threshold': 0.65,
+                  'default_response': 'Я не совсем тебя понимаю.'
+              }])
 
+bot.set_trainer(ChatterBotCorpusTrainer)
+bot.train("../corpus")
 
-# Uncomment the following line to enable verbose logging
-# logging.basicConfig(level=logging.INFO)
-
-# Create a new instance of a ChatBot
-bot = ChatBot("Terminal",
-    storage_adapter="chatterbot.storage.JsonFileStorageAdapter",
-    # logic_adapters=[
-    #     "chatterbot.logic.MathematicalEvaluation",
-    #     "chatterbot.logic.TimeLogicAdapter",
-    #     "chatterbot.logic.BestMatch"
-    # ],
-    # input_adapter="chatterbot.input.TerminalAdapter",
-    # output_adapter="chatterbot.output.TerminalAdapter",
-    database="./database.json"
-)
-
-print("Type something to begin...")
-
-# The following loop will execute each time the user enters input
 while True:
     try:
         answer = input("Я: ")
-        # We pass None to this method because the parameter
-        # is not used by the TerminalAdapter
         bot_input = bot.get_response(answer)
         print("Р: {}".format(bot_input))
 
-    # Press ctrl-c or ctrl-d on the keyboard to exit
     except (KeyboardInterrupt, EOFError, SystemExit):
         break
--- speech_ai.py
+++ speech_ai.py
@@ -1,63 +1,92 @@
-# -*- coding: utf-8 -*-
-
-# Speech recognition and text to speech libraries
+# Библиотеки распознавания и синтеза речи
 import speech_recognition as sr
 from gtts import gTTS
 
-# Library to play mp3
-from pygame import mixer 
+# Воспроизведение речи
+import pygame
+from pygame import mixer
 mixer.init()
 
-# Microphone and recognition
-r = sr.Recognizer()
-m = sr.Microphone()
+import os
+import time
 
-# # Chatterbot library for simple ai
+# Библиотека Chatterbot для простого лингвистического ИИ
+# https://github.com/gunthercox/ChatterBot
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 import logging
-bot = ChatBot(name="Andrew",
-    storage_adapter="chatterbot.storage.JsonFileStorageAdapter",
-    filters=["chatterbot.filters.RepetitiveResponseFilter"],
-    database="./database.json"
-    )
-bot.set_trainer(ChatterBotCorpusTrainer)
-bot.train("corpus")
 
-def make_answer(statement):
-    return bot.get_response(statement)
+class Speech_AI:
+    def __init__(self):
+        self._recognizer = sr.Recognizer()
+        self._microphone = sr.Microphone()
+
+        self.bot = ChatBot(name="Robby",
+            storage_adapter="chatterbot.storage.JsonFileStorageAdapter",
+            filters=["chatterbot.filters.RepetitiveResponseFilter"],
+            database="./database.json"
+        )
+
+        self.bot.set_trainer(ChatterBotCorpusTrainer)
+        self.bot.train("corpus", "chatterbot.corpus.russian")
+        print("Обучение завершено")
+
+        self._mp3_name = "speech.mp3"
 
 
-try:
-    print("A moment of silence, please...")
-    with m as source: r.adjust_for_ambient_noise(source)
-    print("Set minimum energy threshold to {}".format(r.energy_threshold))
-    while True:
-        print("Say something!")
-        with m as source: audio = r.listen(source)
-        print("Got it! Now to recognize it...")
+    def work(self):
+        print("Минутку тишины, пожалуйста...")
+        with self._microphone as source:
+            self._recognizer.adjust_for_ambient_noise(source)
+
         try:
-            # recognize speech using Google Speech Recognition
-            # todo check are there multiple language choise
-            statement = r.recognize_google(audio, language="ru_RU") 
-            
-            # Create answer
-            answer = make_answer(statement)
+            while True:
+                print("Скажи что - нибудь!")
+                with self._microphone as source:
+                    audio = self._recognizer.listen(source)
+                print("Понял, идет распознавание...")
+                try:
+                    statement = self._recognizer.recognize_google(audio, language="ru_RU")
 
-            # Synthesize speech from bot answer
-            tts=gTTS(text=str(answer), lang="ru")
-            tts.save("speach.mp3")
+                    answer = self.make_answer(statement)
+                    # Союда можно добавить много интересностей (IoT, CV, ...)
 
-            # Play answer
-            mixer.music.load('speach.mp3')
-            mixer.music.play()
+                    self.say(str(answer))
+                    while pygame.mixer.music.get_busy():
+                        time.sleep(0.1)
 
-            print("You said {}".format(statement))
-            print("Robot said {}".format(answer))
-        except sr.UnknownValueError:
-            print("Oops! Didn't catch that")
-        except sr.RequestError as e:
-            print("Uh oh! Couldn't request results from Google Speech Recognition service; {0}".format(e))
-except KeyboardInterrupt:
-    bot.trainer.export_for_training('./corpus/last_session_corpus.json')
-    pass
+                    print("Вы сказали: {}".format(statement))
+                    print("{} сказал: {}".format(self.bot.name, answer))
+                except sr.UnknownValueError:
+                    print("Упс! Кажется, я тебя не понял")
+                except sr.RequestError as e:
+                    print("Не могу получить данные от сервиса Google Speech Recognition; {0}".format(e))
+        except KeyboardInterrupt:
+            # Сохраняем данные для следующей сессии
+            self.bot.trainer.export_for_training('corpus/last_session_corpus.json')
+            self._clean_up()
+            print("Пока!")
+
+
+    def say(self, phrase):
+        tts = gTTS(text=phrase, lang="ru")
+        tts.save("speech.mp3")
+
+        # Play answer
+        mixer.music.load('speech.mp3')
+        mixer.music.play()
+
+
+    def make_answer(self, statement):
+        return self.bot.get_response(statement)
+
+    def _clean_up(self):
+        def clean_up():
+            os.remove(self._mp3_name)
+
+
+def main():
+    ai = Speech_AI()
+    ai.work()
+
+main()
